864d86f71f998481586def4679e4233b440f56e1
Hasan Khan <slashdot10@gmail.com>
2015-01-25 05:54:39 -0800

    Added icons and periodic reset

--- myscript.js
+++ myscript.js
@@ -1,20 +1,25 @@
+var jewel = document.getElementById('fbNotificationsJewel');
+var jewelCount1 = document.getElementById('notificationsCountValue');
+var jewelCount2 = document.querySelector('.jewelCount span');
+
 function resetCount() {
 	// change color of globe
-	var jewel = document.getElementById('fbNotificationsJewel');
-	jewel.className = jewel.className.replace(/hasNew/, '');
+	if (jewel.className.indexOf('hasNew') > -1) {
+		jewel.className = jewel.className.replace(/hasNew/, '');
+	}
 
 	// remove count from globe
-	jewlCount = document.getElementById('notificationsCountValue');
-	jewlCount.className += ' hidden_elem';
+	if (jewelCount1.className.indexOf('hidden_elem') == -1) {
+		jewelCount1.className += ' hidden_elem';
+	}
 
 	// remove count from somewhere else
-	var jewlCount = document.querySelector('.jewelCount span');
-	jewlCount.className += ' hidden_elem';
+	if (jewelCount2.className.indexOf('hidden_elem') == -1) {
+		jewelCount2.className += ' hidden_elem';
+	}
 
 	// remove count from window title
-	document.title = document.title.replace(/\(\d+\) /, '')
+	document.title = document.title.replace(/\(\d+\) /, '');
 }
 
-for (var i=0; i<5; i++) {
-	setTimeout(resetCount, 1000*i);
-}
+setInterval(resetCount, 1000);
