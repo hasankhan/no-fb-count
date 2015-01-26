e52fb13ca6677c6164bb17ef7dc1535a8b66efaa
Hasan Khan <slashdot10@gmail.com>
2015-01-25 18:26:47 -0800

    remove the notification flyout

--- myscript.js
+++ myscript.js
@@ -1,21 +1,21 @@
 var jewel = document.getElementById('fbNotificationsJewel');
-var jewelCount1 = document.getElementById('notificationsCountValue');
-var jewelCount2 = document.querySelector('.jewelCount span');
+var jewelButton = document.querySelector('#fbNotificationsJewel .jewelButton');
+var jewelCount = document.getElementById('notificationsCountValue');
+var notificationsFlyout = document.getElementById('fbNotificationsFlyout');
+
 
 function resetCount() {
+	// remove the flyout
+	notificationsFlyout.remove();
+
 	// change color of globe
 	if (jewel.className.indexOf('hasNew') > -1) {
 		jewel.className = jewel.className.replace(/hasNew/, '');
 	}
 
 	// remove count from globe
-	if (jewelCount1.className.indexOf('hidden_elem') == -1) {
-		jewelCount1.className += ' hidden_elem';
-	}
-
-	// remove count from somewhere else
-	if (jewelCount2.className.indexOf('hidden_elem') == -1) {
-		jewelCount2.className += ' hidden_elem';
+	if (jewelCount.className.indexOf('hidden_elem') == -1) {
+		jewelCount.className += ' hidden_elem';
 	}
 
 	// remove count from window title
